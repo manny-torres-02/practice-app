9b83a301b24250de7f2445743684988ac74f4a55
Manny <manueljosetorres02@gmail.com>
2019-10-05 01:01:38 -0500

    set up a react app to practice concepts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Contact from './Container/Contact'
 
-function App() {
+class App extends Component {
+  state = {
+    contacts : [
+      {name:"Manny", age: 26},
+      {name:"alex", age:20}
+    ]
+  }
+
+render () {
+ // want to show contacts
+/* displayContacts = () => {
+
+}*/
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +23,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> This is a react paragraph</p>
+        <Contact> </Contact>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,5 +37,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/Container/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const contacts =(props) => {
+
+    return (
+      <div>
+        <p>I am {this.contact.name} and I am {this.contact.age}</p>
+      </div>
+    )
+
+  
+}
+
+export default contacts;
